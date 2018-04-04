44749540fc8bb8c9c2eed85ef36eddef452e4fdb
David Cheng <dkcheng10@gmail.com>
2018-04-03 18:07:03 -0700

    added constroctor props

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import imageHolder from './Components/images.js'
+import score from './Components/score.js'
 
 //import from game js
 //import from logic js
 
 class App extends Component {
+        constructor(props) {
+          super(props);
+          this.state = {
+            score: 0,
+            highScore: 0,
+          }
+        }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Clicky Game</h1>
           <div className="Message"></div>
-          <div className="score"></div>
-          <div className="topScore"></div>
+          <div className="score"> Score: </div>
+          <div className="topScore">Top Score: </div>
         </header>
         <div className="jumbotron"> <h2>Clicky Game</h2> <p>Click on an image to earn points, but don't click on any more than once!</p></div>
          <div className="imageHolder"> {imageHolder}</div>
--- clickygame/src/Components/score.js
+++ clickygame/src/Components/score.js
@@ -1,1 +1,5 @@
 import React, { Component } from 'react';
+
+const Score = (props) => <div>{props.value}</div>;
+
+export default Score;
